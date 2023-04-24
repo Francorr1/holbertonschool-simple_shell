a44c2df44df61767ef5170ade5547a91ababe143
Franco <francorr240@gmail.com>
2023-04-24 12:22:53 -0700

    Fixed betty

--- tok.c
+++ tok.c
@@ -1,12 +1,15 @@
 #include "main.h"
-/*
-*
+/**
+ * tok_line - Separates the input into tokens
+ * @line: The input line
+ *
+ * Return: Said tokens
 */
 
 char **tok_line(char *line)
 {
 	int bufsize = BUFFER_SIZE, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char*));
+	char **tokens = malloc(bufsize * sizeof(char *));
 	char *token;
 
 	if (!tokens)
@@ -20,12 +23,12 @@ while (token != NULL)
 {
 	tokens[position] = token;
 	position++;
-	
+
 	if (position >= bufsize)
 	{
 		bufsize += BUFFER_SIZE;
-		tokens = realloc(tokens, bufsize * sizeof(char*));
-		
+		tokens = realloc(tokens, bufsize * sizeof(char *));
+
 if (!tokens)
 {
 	fprintf(stderr, "insertar error\n");
