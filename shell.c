587a805203da729cee57bc0faeaf541bedd402a5
Franco <francorr240@gmail.com>
2023-04-25 19:14:31 -0700

    Fixed betty

--- shell.c
+++ shell.c
@@ -1,29 +1,31 @@
 #include "main.h"
-/*
-*
-*/
+/**
+ * main - A simple linux shell
+ *
+ * Return: EXIT_SUCCESS on success
+ */
 
 int main(void)
 {
-    char **args;
-    int status;
-    char *lineptr;
-    size_t n = 0;
-    ssize_t bufsize;
+	char **args;
+	int status;
+	char *lineptr;
+	size_t n = 0;
+	ssize_t bufsize;
 
-    do {
-        printf("$ ");
-        bufsize = getline(&lineptr, &n, stdin);
-        if (bufsize == -1)
-        {
-            return (-1);
-        }
-        args = tok_line(lineptr);
-        status = exec(args);
-    } while (status);
-    
-        free(lineptr);
-        free(args);
+	do {
+		printf("$ ");
+		bufsize = getline(&lineptr, &n, stdin);
+		if (bufsize == -1)
+		{
+			return (-1);
+		}
+		args = tok_line(lineptr);
+		status = exec(args);
+	} while (status);
 
-    return EXIT_SUCCESS;
+	free(lineptr);
+	free(args);
+
+	return (EXIT_SUCCESS);
 }
