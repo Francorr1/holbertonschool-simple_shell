233a0f007bc5dc3e4ac10cadbe5670ab5bc51140
Franco <francorr240@gmail.com>
2023-04-30 00:16:08 -0300

    Fixed return status on not-interactive mode and added AUTHORS file

--- shell.c
+++ shell.c
@@ -12,7 +12,7 @@ int main(void)
 	char *lineptr = NULL;
 	size_t n = 0;
 	ssize_t bufsize;
-	int interactive = isatty(STDIN_FILENO);
+	int interactive = isatty(STDIN_FILENO), return_status = 0;
 
 	do {
 		if (interactive == 1)
@@ -23,23 +23,25 @@ int main(void)
 		if (bufsize == -1)
 		{
 			free(lineptr);
-			return (EXIT_SUCCESS);
+			return (return_status);
 		}
 		args = tok_line(lineptr);
 		if (_strcmp(args[0], "exit") == 0 && args[1] == NULL)
 		{
 			free_grid(args);
 			free(lineptr);
-			exit(EXIT_SUCCESS);
+			exit(return_status);
 		}
 		status = exec(args);
+		if (status != 1)
+			return_status = 2;
 		free(args);
 		if (interactive == 0 && bufsize == -1)
 		{
 			free(lineptr);
-			exit(EXIT_SUCCESS);
+			exit(return_status);
 		}
 	} while (status);
 
-	return (EXIT_SUCCESS);
+	return (return_status);
 }
